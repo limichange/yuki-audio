119ee307692724bb9b79623e27f2c534ae430d63
limichange <limichange@hotmail.com>
2017-02-25 22:08:15 +0800

    🚧  - Work in progress.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-let yukiAudio = {}
+class YukiAudio {
+  constructor (config) {
+    // TODO
+    console.log('YukiAudio')
 
-export default yukiAudio
+    this.audioList = Array.isArray(config.audioList)
+      ? config.audioList
+      : [config.audioList]
+
+    if (config.auto) {
+      this.play()
+    }
+
+    this.audioDom = {}
+  }
+
+  //
+  // Play audio
+  //
+  play () {
+    // TODO
+  }
+
+  //
+  // Stop audio
+  //
+  stop () {
+    // TODO
+    this.audioList = []
+  }
+
+  pause () {
+    // TODO
+  }
+}
+
+export default YukiAudio
